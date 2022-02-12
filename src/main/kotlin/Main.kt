e6ff610fae4017dd24f499adba666712979ea498
Caroline-Kabarungi <josephkabarungicaroline@gmail.com>
2022-02-12 09:06:17 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main (){
+        var Name = "Kabarungi caroline"
+        println(Name)
+        var Age = "20"
+        println(Age)
+        var Phonenumber = "0771420924"
+        println(Phonenumber)
+        var Weight = "59"
+        println(Weight)
+        val isKenyan:Boolean =true
+        println(isKenyan) //output true
+        val isnotKenyan:Boolean =false
+        println(notKenyan) //output false
+
+}
+
+
